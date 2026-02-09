d4c8ecadc796166c65599bc5c013f840de007e34
jcab07 <jcab07@gmail.com>
2026-02-09 10:48:26 +0100

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,2 +1,35 @@
-self.addEventListener('install', () => self.skipWaiting());
-self.addEventListener('activate', (e) => e.waitUntil(self.clients.claim()));
+const CACHE = "inspeccion-flota-v3";
+
+self.addEventListener("install", (event) => {
+  self.skipWaiting();
+  event.waitUntil(
+    caches.open(CACHE).then((c) => c.addAll([
+      "./",
+      "./index.html",
+      "./manifest.json",
+      "./icon-192.png",
+      "./icon-512.png"
+    ]))
+  );
+});
+
+self.addEventListener("activate", (event) => {
+  event.waitUntil(
+    caches.keys().then(keys =>
+      Promise.all(keys.filter(k => k !== CACHE).map(k => caches.delete(k)))
+    ).then(() => self.clients.claim())
+  );
+});
+
+// ✅ Network-first: siempre intenta traer lo último
+self.addEventListener("fetch", (event) => {
+  event.respondWith(
+    fetch(event.request, { cache: "no-store" })
+      .then((res) => {
+        const copy = res.clone();
+        caches.open(CACHE).then((c) => c.put(event.request, copy));
+        return res;
+      })
+      .catch(() => caches.match(event.request))
+  );
+});
